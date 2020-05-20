3e4bc06c773e1db946b2815cf3768b5ff5c78b92
vmolokov <vmolokov59@gmail.com>
2020-05-20 23:48:38 +0500

    Создание приложения

--- MySecondApp/ViewController.swift
+++ MySecondApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var showLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        showLightButton.layer.cornerRadius = 10
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
     }
-
-
+    
+    @IBAction func showLightButtonPressed() {
+        
+    }
 }
 
