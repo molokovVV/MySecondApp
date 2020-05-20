d287ea548e0e301bea380bf8137eb63d202da6dc
vmolokov <vmolokov59@gmail.com>
2020-05-20 18:54:59 +0500

    Initial Commit

---
+++ MySecondApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySecondApp
+//
+//  Created by Виталик Молоков on 20.05.2020.
+//  Copyright © 2020 Виталик Молоков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
